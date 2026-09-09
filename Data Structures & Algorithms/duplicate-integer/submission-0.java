1a9d5d20e4d89c4ba20d90c94b0e6b5586b77cad
Ashutosh Soni <42529126+soni2107@users.noreply.github.com>
2026-09-09 13:10:42 +0530

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums) {
-            if(set.add(i))
-                continue;
-            else
+        //Create a hashmap with key as num entries and value as its frequency
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        //Loop over the array and put unavailable keys in the hashmap
+        for(int num : nums) {
+            //If a key is found in the hashmap then we return true as duplicate exists
+            if(frequencyMap.containsKey(num))
                 return true;
+            
+            frequencyMap.put(num, 1);
         }
+
+        //If no duplicates found we return false;
         return false;
     }
 }
