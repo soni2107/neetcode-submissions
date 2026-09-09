13e6fce68c37b52a6273f79fde6ee315cac76e87
Ashutosh Soni <42529126+soni2107@users.noreply.github.com>
2026-09-09 14:58:50 +0530

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // We create a hashmap where the key will target - num value and the value will be index of num value
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length ; i++) {
-            if(map.containsKey(target - nums[i]))
-                return new int[]{map.get(target - nums[i]), i};
-            else
-                map.put(nums[i], i);
+        //We loop over the nums array and check if num exists in the hashmap
+        for(int i = 0; i < nums.length; i++) {
+            //If it does then it means we have its complimentary number the sum of whom with num will be equal to target
+            // we return the index of both of them
+            if(map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
         }
+        
+        //else we return -1, -1
         return new int[]{-1, -1};
     }
 }
