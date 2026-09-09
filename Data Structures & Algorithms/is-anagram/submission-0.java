a45e94bdcda459e92607beb3c427fa6ad97da0de
Ashutosh Soni <42529126+soni2107@users.noreply.github.com>
2026-09-09 13:22:56 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,18 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        //Initial check for length match of s and t
         if(s.length() != t.length())
             return false;
+        
+        //Create an array of 26 length corresponding to 26 letters in the alphabet
+        int[] alpArray = new int[26];
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-        //Check if I can do this via streaming API
+        //Loop over the string characters and increase value of array index with s char and reduce it with t char
+        for(int i = 0; i < s.length(); i++) {
+            int sIndex = s.charAt(i) - 'a';
+            int tIndex = t.charAt(i) - 'a';
 
-        for(int i = 0; i < sArray.length; i++) {
-            if(sArray[i] != tArray[i])
+            alpArray[sIndex] += 1;
+            alpArray[tIndex] -= 1;
+        }
+
+        //Check the values in the array and if all are 0 then anagram else it is not
+        for(int val : alpArray) {
+            if(val != 0)
                 return false;
         }
+        
         return true;
     }
 }
